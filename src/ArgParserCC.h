72e4a04c4cb50c0c493115cd0e28fd2ee8f5d7a2
Malcolm Hall <malcolmrainehall@gmail.com>
2024-03-20 20:50:38 -0400

    Reformat code

--- src/ArgParserCC.h
+++ src/ArgParserCC.h
@@ -36,45 +36,45 @@
 
 class Argument {
 public:
- Argument(const std::string& key, const std::string& description, bool has_value);
- void SetAlternateKeys(const std::vector<std::string>& keys);
- std::vector<std::string> GetAlternateKeys();
- [[nodiscard]] std::string GetKey() const;
- [[nodiscard]] std::string GetValue() const;
- void SetValue(const std::string& value);
- [[nodiscard]] std::string GetDescription() const;
- [[nodiscard]] bool IsSet() const;
- [[nodiscard]] bool HasValue() const;
+    Argument(const std::string& key, const std::string& description, bool has_value);
+    void SetAlternateKeys(const std::vector<std::string>& keys);
+    std::vector<std::string> GetAlternateKeys();
+    [[nodiscard]] std::string GetKey() const;
+    [[nodiscard]] std::string GetValue() const;
+    void SetValue(const std::string& value);
+    [[nodiscard]] std::string GetDescription() const;
+    [[nodiscard]] bool IsSet() const;
+    [[nodiscard]] bool HasValue() const;
 
 private:
- std::string _key;
- std::string _value;
- bool _is_set = false;
- std::string _description;
- bool _has_value = false;
- std::vector<std::string> _alternates_keys{};
+    std::string _key;
+    std::string _value;
+    bool _is_set = false;
+    std::string _description;
+    bool _has_value = false;
+    std::vector<std::string> _alternates_keys{};
 };
 
 class ArgParserCC {
 public:
-  ArgParserCC();
- void SetMainDescription(const std::string& description);
- void AddArgument(const std::string& arg, const std::vector<std::string>& alternates,
-                  const std::string& description, bool has_value = false,
-                  const std::string& defaultValue = "");
- [[nodiscard]] bool IsSet(const std::string& arg);
- [[nodiscard]] std::string GetValue(const std::string& arg);
- void ShowHelp();
- void Parse(int argc, char* argv[]);
- [[nodiscard]] std::string GetInputFile() const;
- std::vector<std::string> GetProgramArguments();
+    ArgParserCC();
+    void SetMainDescription(const std::string& description);
+    void AddArgument(const std::string& arg, const std::vector<std::string>& alternates,
+                     const std::string& description, bool has_value = false,
+                     const std::string& defaultValue = "");
+    [[nodiscard]] bool IsSet(const std::string& arg);
+    [[nodiscard]] std::string GetValue(const std::string& arg);
+    void ShowHelp();
+    void Parse(int argc, char* argv[]);
+    [[nodiscard]] std::string GetInputFile() const;
+    std::vector<std::string> GetProgramArguments();
 
 private:
- std::map<std::string, Argument*> _arguments{};
- std::string _main_description;
- std::string _input_file;
- std::unordered_set<std::string> _help_exclusions{};
- std::vector<std::string> _ordered_args{};
- size_t _longest_key_length = 0;
- std::vector<std::string> _program_arguments;
+    std::map<std::string, Argument*> _arguments{};
+    std::string _main_description;
+    std::string _input_file;
+    std::unordered_set<std::string> _help_exclusions{};
+    std::vector<std::string> _ordered_args{};
+    size_t _longest_key_length = 0;
+    std::vector<std::string> _program_arguments;
 };
