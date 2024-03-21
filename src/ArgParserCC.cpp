1f58bfc6cfa01d69bda2b10f8a5f5a2d4ced9a52
Malcolm Hall <malcolmrainehall@gmail.com>
2024-03-20 20:27:45 -0400

    Initial commit of arg parser library

---
+++ src/ArgParserCC.cpp
@@ -0,0 +1,179 @@
+/*****************************************************************************
+* File: ArgParserCC.cpp
+* Description:
+* Author: Malcolm Hall
+* Date: 3/20/24
+* Version: 1
+*
+* MIT License
+*
+* Copyright (c) 2024 - 2024 Malcolm Hall
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ *****************************************************************************/
+
+#include "ArgParserCC.h"
+#include <iomanip>
+#include <iostream>
+
+#define HELP_STRING_COLUMN_PAD (10)
+
+/*****************************************************************************/
+Argument::Argument(const std::string& key, const std::string& description,
+                   bool has_value)
+    : _key(key), _description(description), _has_value(has_value) {}
+
+/*****************************************************************************/
+void Argument::SetAlternateKeys(const std::vector<std::string>& keys) {
+  _alternates_keys = keys;
+}
+
+/*****************************************************************************/
+std::vector<std::string> Argument::GetAlternateKeys() {
+  return _alternates_keys;
+}
+
+/*****************************************************************************/
+std::string Argument::GetKey() const {
+  return _key;
+}
+
+/*****************************************************************************/
+std::string Argument::GetValue() const {
+  return _value;
+}
+
+/*****************************************************************************/
+void Argument::SetValue(const std::string& value) {
+  _value = value;
+  _is_set = true;
+}
+
+/*****************************************************************************/
+std::string Argument::GetDescription() const {
+  return _description;
+}
+
+/*****************************************************************************/
+bool Argument::IsSet() const {
+  return _is_set;
+}
+
+/*****************************************************************************/
+bool Argument::HasValue() const {
+  return _has_value;
+}
+
+/*****************************************************************************/
+/*****************************************************************************/
+ArgParserCC::ArgParser() = default;
+
+/*****************************************************************************/
+void ArgParserCC::SetMainDescription(const std::string& description) {
+  _main_description = description;
+}
+
+/*****************************************************************************/
+void ArgParserCC::AddArgument(const std::string& arg,
+                            const std::vector<std::string>& alternates,
+                            const std::string& description, bool has_value,
+                            const std::string& defaultValue) {
+  auto argument = new Argument(arg, description, has_value);
+  _arguments[arg] = argument;
+  _longest_key_length =
+      _longest_key_length > arg.size() ? _longest_key_length : arg.size();
+  _ordered_args.push_back(arg);
+
+  if (!defaultValue.empty()) {
+    argument->SetValue(defaultValue);
+  }
+
+  argument->SetAlternateKeys(alternates);
+  for (const auto& key : alternates) {
+    _arguments[key] = argument;
+    _help_exclusions.insert(key);
+  }
+}
+
+/*****************************************************************************/
+bool ArgParserCC::IsSet(const std::string& arg) {
+  if (_arguments.contains(arg)) {
+    return _arguments[arg]->IsSet();
+  }
+  return false;
+}
+
+/*****************************************************************************/
+std::string ArgParserCC::GetValue(const std::string& arg) {
+  return _arguments[arg]->GetValue();
+}
+
+/*****************************************************************************/
+void ArgParserCC::ShowHelp() {
+  std::string help;
+  help.append(_main_description + "\n");
+  for (const auto& key : _ordered_args) {
+    if (_help_exclusions.find(key) != _help_exclusions.end()) {
+      continue;
+    }
+
+    auto argument = _arguments[key];
+    std::string key_str = key;
+    for (const auto& alternate_key : argument->GetAlternateKeys()) {
+      key_str.append(", " + alternate_key);
+    }
+    help.append(
+        "  " +
+        String::Pad(key_str, _longest_key_length + HELP_STRING_COLUMN_PAD, ' ') +
+        argument->GetDescription() + "\n");
+  }
+
+  std::cout << help;
+}
+
+/*****************************************************************************/
+void ArgParserCC::Parse(int argc, char* argv[]) {
+  for (int i = 1; i < argc; i++) {
+    auto arg_str = std::string(argv[i]);
+    if (_input_file.empty()) {
+      if (_arguments.contains(arg_str)) {
+        auto argument = _arguments[arg_str];
+        if (argument->HasValue()) {
+          i++;
+          argument->SetValue(std::string(argv[i]));
+        } else {
+          // Need to do this to toggle the set flag
+          argument->SetValue("");
+        }
+      } else if (arg_str.ends_with(".rn") || arg_str.ends_with(".rnc")) {
+        _input_file = arg_str;
+      }
+    } else {
+      _program_arguments.push_back(arg_str);
+    }
+  }
+}
+
+/*****************************************************************************/
+std::string ArgParserCC::GetInputFile() const {
+  return _input_file;
+}
+
+/*****************************************************************************/
+std::vector<std::string> ArgParserCC::GetProgramArguments() {
+  return _program_arguments;
+}
---
+++ src/ArgParserCC.h
@@ -0,0 +1,80 @@
+/*****************************************************************************
+* File: ArgParserCC.h
+* Description:
+* Author: Malcolm Hall
+* Date: 3/20/24
+* Version: 1
+*
+* MIT License
+*
+* Copyright (c) 2024 - 2024 Malcolm Hall
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*****************************************************************************/
+
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+class Argument {
+public:
+ Argument(const std::string& key, const std::string& description, bool has_value);
+ void SetAlternateKeys(const std::vector<std::string>& keys);
+ std::vector<std::string> GetAlternateKeys();
+ [[nodiscard]] std::string GetKey() const;
+ [[nodiscard]] std::string GetValue() const;
+ void SetValue(const std::string& value);
+ [[nodiscard]] std::string GetDescription() const;
+ [[nodiscard]] bool IsSet() const;
+ [[nodiscard]] bool HasValue() const;
+
+private:
+ std::string _key;
+ std::string _value;
+ bool _is_set = false;
+ std::string _description;
+ bool _has_value = false;
+ std::vector<std::string> _alternates_keys{};
+};
+
+class ArgParserCC {
+public:
+  ArgParserCC();
+ void SetMainDescription(const std::string& description);
+ void AddArgument(const std::string& arg, const std::vector<std::string>& alternates,
+                  const std::string& description, bool has_value = false,
+                  const std::string& defaultValue = "");
+ [[nodiscard]] bool IsSet(const std::string& arg);
+ [[nodiscard]] std::string GetValue(const std::string& arg);
+ void ShowHelp();
+ void Parse(int argc, char* argv[]);
+ [[nodiscard]] std::string GetInputFile() const;
+ std::vector<std::string> GetProgramArguments();
+
+private:
+ std::map<std::string, Argument*> _arguments{};
+ std::string _main_description;
+ std::string _input_file;
+ std::unordered_set<std::string> _help_exclusions{};
+ std::vector<std::string> _ordered_args{};
+ size_t _longest_key_length = 0;
+ std::vector<std::string> _program_arguments;
+};
